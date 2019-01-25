6c8f7b224c4c6e12dc7238519c24a57361afd46d
APCS2nd <APCS2nd@MHS-Dreyer-08>
2019-01-25 09:34:27 -0800

    frac calc almost finished

--- FracCalcWithObjects/src/fracCalc/Fraction.java
+++ FracCalcWithObjects/src/fracCalc/Fraction.java
@@ -11,7 +11,6 @@ public class Fraction {
 	private int wholeNum;
 	private int numerator;
 	private int denominator;
-	private int sign;
 	public Fraction(String operand) {
     	if(operand.contains("_")) {
     		String[] wholeAndFrac = operand.split("_");
@@ -32,7 +31,6 @@ public class Fraction {
 		wholeNum = 0;
 		numerator = 0;
 		denominator = 1;
-		sign = 1;
 	}
 	public void toImproperFrac() {
     	if(wholeNum >= 0) {
@@ -42,46 +40,68 @@ public class Fraction {
     	}
     	wholeNum = 0;
     }
-	public String doMath(String operator, Fraction operand) {
-		int gcf = gcf(denominator, operand.getDenominator());
+	public Fraction doMath(String operator, Fraction operand) {
+		Fraction answer = new Fraction();
+		int gcf = gcf(denominator, operand.denominator);
 		if(operator.equals("+")) {
-	    	numerator = (numerator *(gcf/denominator)) + (operand.getNumerator()*(gcf/operand.getDenominator()));
-	    	denominator = gcf;
+	    	answer.numerator = (numerator *(gcf/denominator)) + (operand.numerator*(gcf/operand.denominator));
+	    	answer.denominator = gcf;
 		}else if(operator.equals("-")) {
-			numerator = (numerator *(gcf/denominator)) - (operand.getNumerator()*(gcf/operand.getDenominator()));
-	    	denominator = gcf;
+			answer.numerator = (numerator *(gcf/denominator)) - (operand.numerator*(gcf/operand.denominator));
+	    	answer.denominator = gcf;
 		}else if(operator.equals("*")) {
-			numerator *= operand.getNumerator();
-			denominator *= operand.getDenominator();
+			answer.numerator = numerator * operand.numerator;
+			answer.denominator = denominator * operand.denominator;
 		}else {
-			numerator *= operand.getDenominator();
-			denominator *= operand.getNumerator();
+			answer.numerator = numerator * operand.denominator;
+			answer.denominator = denominator * operand.numerator;
 		}
-		reduce();
-		return toMixedNum();
+		answer = reduce(answer);
+		return toMixedNum(answer);
 	}
 	public String toString() {
-		return wholeNum + "_" + numerator + "/" + denominator;
+		//gets rid of numerator and denominator if numerator = 0
+		String output = wholeNum + "_" + numerator + "/" + denominator;
+		if(output.substring(output.indexOf("_") + 1).startsWith("0")) {
+  			output = output.substring(0, output.indexOf("_"));
+  		}
+		//gets rid of 0 as whole number
+  		if(output.startsWith("0")) { 
+  			output = output.substring(output.indexOf("_") + 1, output.length());
+  		}
+  		//formats the mixed number by getting rid of improper negative signs in the numerator and/or denominator
+  		if(numerator < 0 && denominator < 0 && output.indexOf("_") != -1) {
+  			output = numerator/denominator + "_" + (numerator%denominator)*-1 + "/" + denominator*-1;
+  		}else if(numerator < 0 && denominator > 0 && output.indexOf("_") != -1) {
+  			output = numerator/denominator + "_" + (numerator%denominator)*-1 + "/" + denominator;
+  		}else if(numerator > 0 && denominator < 0 && output.indexOf("_") != -1) {
+  			output = numerator/denominator + "_" + (numerator%denominator) + "/" + denominator*-1;
+  		}else if(denominator < 0 && output.indexOf("_") == -1 && output.indexOf("/") != -1) { 
+  			output = (numerator%denominator)*-1 + "/" + denominator*-1;
+  		}
+		return output;
 	}
 	//determines the greatest common factor of two integers
   	public int gcf(int num1, int num2) {
   		return num1 * num2;
   	}
   	//reduces the fraction by finding the least common denominator and dividing the numbers by that number
-  	public void reduce() {
+  	public Fraction reduce(Fraction frac) {
+  		Fraction reducedFrac = frac;
   		int greaterNum;
   		//uses absolute value so it also works with negative numbers
-  		if(absValue(numerator) > absValue(denominator)) {
-  			greaterNum = absValue(numerator);
+  		if(absValue(reducedFrac.numerator) > absValue(reducedFrac.denominator)) {
+  			greaterNum = absValue(reducedFrac.numerator);
   		}else {
-  			greaterNum = absValue(denominator);
+  			greaterNum = absValue(reducedFrac.denominator);
   		}
   		for(int i = greaterNum; i > 1; i--) {
-  			if(isDivisibleBy(absValue(numerator), i) && isDivisibleBy(absValue(denominator), i)) {
-  				numerator /= i;
-  				denominator /= i;
+  			if(isDivisibleBy(absValue(reducedFrac.numerator), i) && isDivisibleBy(absValue(reducedFrac.denominator), i)) {
+  				reducedFrac.numerator /= i;
+  				reducedFrac.denominator /= i;
   			}
   		}
+  		return reducedFrac;
   	}
   	//returns the absolute value of the number passed
   	public int absValue(int number) {
@@ -100,35 +120,10 @@ public class Fraction {
   		}
   	}
     //converts an improper fraction to a mixed number 
-  	public String toMixedNum() {
-  		String mixedNum = numerator/denominator + "_" + (numerator%denominator) + "/" + denominator;
-  		//gets rid of numerator and denominator if numerator = 0
-  		if(mixedNum.substring(mixedNum.indexOf("_") + 1).startsWith("0")) {
-  			mixedNum = mixedNum.substring(0, mixedNum.indexOf("_"));
-  		}
-  		//gets rid of 0 as whole number
-  		if(mixedNum.startsWith("0")) { 
-  			mixedNum = mixedNum.substring(mixedNum.indexOf("_") + 1, mixedNum.length());
-  		}
-  		//formats the mixed number by getting rid of improper negative signs in the numerator and/or denominator
-  		if(numerator < 0 && denominator < 0 && mixedNum.indexOf("_") != -1) {
-  			mixedNum = numerator/denominator + "_" + (numerator%denominator)*-1 + "/" + denominator*-1;
-  		}else if(numerator < 0 && denominator > 0 && mixedNum.indexOf("_") != -1) {
-  			mixedNum = numerator/denominator + "_" + (numerator%denominator)*-1 + "/" + denominator;
-  		}else if(numerator > 0 && denominator < 0 && mixedNum.indexOf("_") != -1) {
-  			mixedNum = numerator/denominator + "_" + (numerator%denominator) + "/" + denominator*-1;
-  		}else if(denominator < 0 && mixedNum.indexOf("_") == -1 && mixedNum.indexOf("/") != -1) { 
-  			mixedNum = (numerator%denominator)*-1 + "/" + denominator*-1;
-  		}
+  	public Fraction toMixedNum(Fraction frac) {
+  		Fraction mixedNum = frac;
+  		mixedNum.wholeNum = numerator/denominator;
+  		mixedNum.numerator = numerator%denominator;
   		return mixedNum;
   	}
-	public int getWholeNum() {
-		return wholeNum;
-	}
-	public int getNumerator() {
-		return numerator;
-	}
-	public int getDenominator() {
-		return denominator;
-	}
 }
