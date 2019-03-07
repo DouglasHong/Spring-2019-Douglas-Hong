0f50b30a9124b38a5fc71eafecf27200f6697ac4
APCS2nd <APCS2nd@MHS-Dreyer-08>
2019-03-07 09:47:35 -0800

    textcell added

---
+++ TextExcel/src/textExcel/TextCell.java
@@ -0,0 +1,18 @@
+package textExcel;
+
+public class TextCell implements Cell{
+	private String text;
+	public TextCell(String text) {
+		this.text = text;
+	}
+	public String fullCellText() {
+		return ("\" + text + \"");
+	}
+	public String abbreviatedCellText() {
+		String truncated = "";
+		for(int i = 0; i < 10; i++) {
+			truncated += text.charAt(i);
+		}
+		return truncated;
+	}
+}
